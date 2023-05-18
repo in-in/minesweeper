6747a0e6529a2727cd0a2db66a3fdc958c587e3c
in-in <in-in@users.noreply.github.com>
2023-05-18 22:43:12 +0300

    refactor(wrapper): update render method

--- minesweeper/src/components/wrapper/index.js
+++ minesweeper/src/components/wrapper/index.js
@@ -1,9 +1,19 @@
+import { dashboard } from '../dashboard';
+import { field } from '../field';
+import { level } from '../level';
+import { scoretable } from '../scoretable';
+
 const wrapper = {
 	render(container) {
 		const element = document.createElement('div');
 		element.className = 'wrapper';
+
+		dashboard.render(element);
+		field.render(element);
+		level.render(element);
+		scoretable.render(element);
+
 		container.appendChild(element);
-		return element;
 	},
 };
 
--- minesweeper/src/scripts/main.js
+++ minesweeper/src/scripts/main.js
@@ -1,15 +1,3 @@
 import { wrapper } from '../components/wrapper';
-import { dashboard } from '../components/dashboard';
-import { field } from '../components/field';
-import { level } from '../components/level';
-import { scoretable } from '../components/scoretable';
 
-const { body } = document;
-
-wrapper.render(body);
-const wrapperElement = document.querySelector('.wrapper');
-
-dashboard.render(wrapperElement);
-field.render(wrapperElement);
-level.render(wrapperElement);
-scoretable.render(wrapperElement);
+wrapper.render(document.body);
