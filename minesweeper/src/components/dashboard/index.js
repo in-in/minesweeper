bf8b81fff3e4e627657285f15c6ad6140f9393b4
in-in <in-in@users.noreply.github.com>
2023-05-18 22:18:53 +0300

    refactor(toggle): rename & add js

--- minesweeper/src/components/dashboard/index.js
+++ minesweeper/src/components/dashboard/index.js
@@ -1,4 +1,5 @@
 import { range } from '../range';
+import { toggle } from '../toggle';
 
 const dashboard = {
 	render(container) {
@@ -6,6 +7,8 @@ const dashboard = {
 		element.className = 'dashboard';
 
 		range.render(element);
+		toggle.render(element, 'sound');
+		toggle.render(element, 'day/night');
 
 		container.appendChild(element);
 	},
