7849950748c9595a9f5c8ea9e6e502d816456ffc
in-in <in-in@users.noreply.github.com>
2023-07-15 03:47:12 +0000

    feat(wrapper): add wrapper

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 
-import src1 from "@images/nathan.jpg";
-import src2 from "@images/stanos.jpg";
-import trainUrl from "@images/train.svg?url";
-import Bomb from "@svg/bomb.svg";
+import { Wrapper } from "@components/Wrapper";
 
 import st from "./index.module.scss";
 
 const App = (): JSX.Element => (
 	<div className={st.layout}>
-		<p className={st.title}>App</p>
-		<img src={src1} alt="" className={st.img} />
-		<img src={src2} alt="" className={st.img} />
-		<img src={trainUrl} alt="" className={st.img} />
-		<div className={st.back}></div>
-		<div className={st.back1}></div>
-		<div className={st.back2}></div>
-		<Bomb className={st.icon} />
+		<Wrapper />
 	</div>
 );
 
---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import st from "./index.module.scss";
+
+const Wrapper = (): JSX.Element => <div className={st.wrapper}></div>;
+
+export { Wrapper };
