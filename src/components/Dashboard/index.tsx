1e0fd5cf7bbe51ecebf55fcac2bc8c492d8fbc28
in-in <in-in@users.noreply.github.com>
2023-07-15 22:56:19 +0000

    feat(toggle): add Toggle

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import { Range } from "@components/Range";
+import { Toggle } from "@components/Toggle";
 
 import st from "./index.module.scss";
 
 const Dashboard = (): JSX.Element => (
 	<div className={st.dashboard}>
 		<Range />
+		<Toggle label="sound" />
+		<Toggle label="light/dark" />
 	</div>
 );
 
---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import st from "./index.module.scss";
+
+interface AppProps {
+	label: string;
+}
+
+const Toggle = ({ label }: AppProps): JSX.Element => {
+	const id = label.replace(/[^a-z0-9]/gi, "-");
+	return (
+		<div className={st.toggle}>
+			<label className={st.title} htmlFor={`toggle-${id}`}>
+				{label}
+			</label>
+			<input
+				type="checkbox"
+				className={st.input}
+				name={`toggle-${id}`}
+				id={`toggle-${id}`}
+			/>
+		</div>
+	);
+};
+
+export { Toggle };
