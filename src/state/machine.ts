347b28187936eeebd8564ed989695c4cd8f462c5
in-in <in-in@users.noreply.github.com>
2023-07-17 16:37:58 +0000

    feat(state): add xstate

---
+++ src/state/machine.ts
@@ -0,0 +1,44 @@
+import { createMachine } from "xstate";
+
+enum State {
+	"Idle" = "idle",
+	"Play" = "play",
+	"Finish" = "finish",
+}
+
+const machine = createMachine({
+	schema: {
+		// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+		events: {} as { type: "START" } | { type: "WIN" } | { type: "RESTART" },
+	},
+	id: "main",
+	initial: State.Idle,
+	states: {
+		[State.Idle]: {
+			on: {
+				START: {
+					target: State.Play,
+				},
+			},
+		},
+		[State.Play]: {
+			on: {
+				WIN: {
+					target: State.Finish,
+				},
+			},
+		},
+		[State.Finish]: {
+			on: {
+				RESTART: {
+					target: State.Idle,
+				},
+			},
+		},
+	},
+});
+
+const { initialState } = machine;
+console.log(initialState);
+
+export { machine };
