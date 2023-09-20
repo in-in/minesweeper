caea82bfbed8c871cad64fef650b6d292f519dc7
in-in <in-in@users.noreply.github.com>
2023-09-20 14:13:57 +0300

    refactor(Cell): update types

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -2,16 +2,17 @@ import React from "react";
 
 import st from "./index.module.scss";
 
-interface CellProps {
+interface CellProps extends React.ComponentPropsWithoutRef<"button"> {
 	cellRowId: number;
 	cellColId: number;
 }
 
-const Cell = ({ cellRowId, cellColId }: CellProps): React.ReactNode => (
+const Cell = ({ cellRowId, cellColId, ...rest }: CellProps): React.ReactNode => (
 	<button
 		className={st.cell}
 		data-cellrowid={cellRowId}
 		data-cellcolid={cellColId}
+		{...rest}
 	></button>
 );
 
