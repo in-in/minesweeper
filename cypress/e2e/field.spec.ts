e0bb5aac88201b814ec76b361cb9984dc0c0033e
in-in <in-in@users.noreply.github.com>
2023-11-13 16:42:21 +0000

    test(field): check active class in open cell

--- cypress/e2e/field.spec.ts
+++ cypress/e2e/field.spec.ts
@@ -4,10 +4,22 @@ const [value] = Object.values(LEVELS[0]);
 const initialLevel = value * value;
 
 describe("field", () => {
-	it(`field should contain ${initialLevel} buttons`, () => {
+	beforeEach(() => {
 		cy.visit("/");
+	});
+
+	it(`field should contain ${initialLevel} buttons`, () => {
 		cy.getByTestId("field").within(() => {
 			cy.get("button").should("have.length", initialLevel);
+		});
+	});
+
+	it("active class must be set to open cell", () => {
+		cy.start();
+		cy.getByTestId("field").within(() => {
+			cy.get("button")
+				.eq(0)
+				.should(($btn) => expect($btn[0].className).to.match(/open_/));
 		});
 	});
 });
