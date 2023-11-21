3842887473f7b77ba1a24dc9df3a1836c3b6c608
in-in <in-in@users.noreply.github.com>
2023-11-21 15:15:51 +0000

    test(field): highlight surrounding cells

--- cypress/e2e/field.spec.ts
+++ cypress/e2e/field.spec.ts
@@ -6,20 +6,41 @@ const initialLevel = value * value;
 describe("field", () => {
 	beforeEach(() => {
 		cy.visit("/");
+		cy.start();
+		cy.getByTestId("field").as("field");
 	});
 
 	it(`field should contain ${initialLevel} buttons`, () => {
-		cy.getByTestId("field").within(() => {
+		cy.get("@field").within(() => {
 			cy.get("button").should("have.length", initialLevel);
 		});
 	});
 
 	it("active class must be set to open cell", () => {
-		cy.start();
-		cy.getByTestId("field").within(() => {
+		cy.get("@field").within(() => {
 			cy.get("button")
 				.eq(0)
 				.should(($btn) => expect($btn[0].className).to.match(/open_/));
+		});
+	});
+
+	it("highlight surrounding cells", () => {
+		cy.get("@field").within(() => {
+			cy.get("button").eq(0).trigger("mousedown", { button: 1 });
+			cy.get("button:nth-child(2), button:nth-child(11), button:nth-child(12)").as(
+				"btns",
+			);
+			cy.get("@btns").each(($el) => {
+				cy.wrap($el)
+					.invoke("attr", "class")
+					.should("to.match", /highlight_/);
+			});
+			cy.get("button").eq(0).trigger("mouseup", { button: 1 });
+			cy.get("@btns").each(($el) => {
+				cy.wrap($el)
+					.invoke("attr", "class")
+					.should("not.to.match", /highlight_/);
+			});
 		});
 	});
 });
