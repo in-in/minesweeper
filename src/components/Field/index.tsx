45dacb680cdcaa89037305abcf298ec05a91f974
in-in <in-in@users.noreply.github.com>
2023-12-22 08:07:29 +0000

    refactor(field): remove useless custom properties and style file

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -1,22 +1,30 @@
-import { type CSSCustomProperties } from "@/customTypes/customTypes";
+import { Box, type SxProps } from "@mui/material";
 
 import { Cell as FieldCell } from "@/components/Cell";
 import { selectCurrentLevelSize, selectField } from "@/store/selectors";
 import { useAppSelector } from "@/utils/hooks";
 
-import st from "./index.module.scss";
-
 const Field = (): React.ReactNode => {
 	const currentLevelSize = useAppSelector(selectCurrentLevelSize);
 	const field = useAppSelector(selectField);
-	const style: CSSCustomProperties = { "--size": currentLevelSize };
+
+	const styles: SxProps = {
+		display: "grid",
+		width: "100%",
+		maxWidth: "100%",
+		overflow: "hidden",
+		aspectRatio: 1,
+		fontSize: currentLevelSize === 10 ? 24 : 16,
+		gridTemplateRows: `repeat(${currentLevelSize}, minmax(0, 1fr))`,
+		gridTemplateColumns: `repeat(${currentLevelSize}, minmax(0, 1fr))`,
+	};
 
 	return (
-		<div className={st.field} data-testid="field" style={style}>
+		<Box data-testid="field" sx={styles}>
 			{field.map((cell) => (
 				<FieldCell cell={cell} key={cell.id} />
 			))}
-		</div>
+		</Box>
 	);
 };
 
